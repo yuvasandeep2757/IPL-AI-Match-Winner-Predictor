2d30bb2d697cf549b7ab98810ba1edcb60daa690
Molli Yuva Sandeep <yuvasandeepmolli34@gmail.com>
2026-06-18 20:59:17 +0530

    Fixed API route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
     setPrediction(null);
 
     try {
-      const response = await fetch('https://yuvasandeep1934.pythonanywhere.com/', {
+      const response = await fetch('https://yuvasandeep1934.pythonanywhere.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ team1, team2, toss_winner: tossWinner, venue })
